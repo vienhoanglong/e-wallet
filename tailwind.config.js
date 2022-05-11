dd51ea2b9d10693fb3aa585ed55532c5dcf71061
vienhoanglong <Vienhoanglong789@gmail.com>
2022-05-11 14:31:02 +0700

    logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   mode:'jit',
   purge:["./**/*.html"],
+  plugins: [
+    require('tailwindcss-textshadow')
+  ],
   theme: {
     fontFamily:{
       poppins: ["Poppins"],
@@ -11,7 +14,11 @@ module.exports = {
       }
     },
     boxShadow:{
-      "3d": "6px 6px 16px 0 rgba(0, 0, 0, 0.25), -4px -4px 12px 0 rgba(255, 255, 255, 0.3);"
+      "3d": "6px 6px 16px 0 rgba(0, 0, 0, 0.25), -4px -4px 12px 0 rgba(255, 255, 255, 0.3);",
+    },
+    textShadow:{
+      't1': '0 2px 0 #000',
+      't2': '5px 7px #000000'
     }
   },
 }
